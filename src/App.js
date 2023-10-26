ea47d3198a9e5dc3705b7d3b379a01e3778f3eb3
Lamnc <lamnc@arrowhitech.co>
2023-10-26 09:15:23 +0700

    Update-Pagination-Custom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect} from 'react';
+import {
+  Navigate,
+  Routes,
+  Route
+} from "react-router-dom";
+import ProductFeature from './features/Product';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Lam Nguyen
-        </p>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products/" element={<ProductFeature />} />
+      </Routes>
     </div>
   );
 }
